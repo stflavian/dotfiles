21c2c938ce43caab9d4e09e8a783ce396e4d3ac1
Flavian Stavarache <FlavianStavarache@outlook.com>
2024-03-28 11:46:52 +0100

    added git integrations to telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,2 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
